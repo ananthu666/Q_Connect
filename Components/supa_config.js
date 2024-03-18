dbf3b47af611763fa2635aaa10a366f6ce091961
Ananthu Jayakumar <115489088+ananthu666@users.noreply.github.com>
2024-03-18 16:44:37 +0530

    supa

---
+++ Components/supa_config.js
@@ -0,0 +1,8 @@
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://prywlsdmlqxtebftnhuq.supabase.co";
+const supabaseKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByeXdsc2RtbHF4dGViZnRuaHVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NTg1MTQsImV4cCI6MjAyNjMzNDUxNH0.lCvV4JLizSwymLLjbSsH5Ypw_G6cNxVcm0r9t-HqrVo";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
